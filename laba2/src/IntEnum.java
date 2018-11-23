bc6593b03e27f1dc8fccb955a609ed5fd7255705
MariaDron <maria.dron0@gmail.com>
2018-11-23 07:55:20 +0200

    LABA-2: refactoring ad optimization

--- laba2/src/IntEnum.java
+++ laba2/src/IntEnum.java
@@ -5,26 +5,7 @@ public enum IntEnum {
     ZERO(0), ONE(1), TWO(2), THREE(3);
     private final int value;
 
-    private IntEnum(int value) {
+    IntEnum(int value) {
         this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public IntEnum copmareValue(int value) {
-        switch (value) {
-            case 0:
-                return ZERO;
-            case 1:
-                return ONE;
-            case 2:
-                return TWO;
-            case 3:
-                return THREE;
-            default:
-                return null;
-        }
     }
 }
